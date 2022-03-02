8aa797ca99d5b08de0bfc095c88d65801c8f081a
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2022-03-02 12:37:22 +0100

    chore(observability): update to v3.0.9
    This allows volume size override and works with OpenShift 4.1

--- internal/dinosaur/internal/workers/clusters_mgr.go
+++ internal/dinosaur/internal/workers/clusters_mgr.go
@@ -37,7 +37,7 @@ import (
 // TODO change these constants to match your own
 const (
 	observabilityNamespace           = "managed-application-services-observability"
-	observabilityCatalogSourceImage  = "quay.io/rhoas/observability-operator-index:v3.0.8"
+	observabilityCatalogSourceImage  = "quay.io/rhoas/observability-operator-index:v3.0.9"
 	observabilityOperatorGroupName   = "observability-operator-group-name"
 	observabilityCatalogSourceName   = "observability-operator-manifests"
 	observabilitySubscriptionName    = "observability-operator"
@@ -842,7 +842,7 @@ func (c *ClusterManager) buildObservabilitySubscriptionResource() *v1alpha1.Subs
 			CatalogSource:          observabilityCatalogSourceName,
 			Channel:                "alpha",
 			CatalogSourceNamespace: observabilityNamespace,
-			StartingCSV:            "observability-operator.v3.0.8",
+			StartingCSV:            "observability-operator.v3.0.9",
 			InstallPlanApproval:    v1alpha1.ApprovalAutomatic,
 			Package:                observabilitySubscriptionName,
 		},
